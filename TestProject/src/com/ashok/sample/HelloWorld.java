2685dbdfc92683ce41985f8601a7e81141cef6d0
AshokRam Adiraju <ashokram.adiraju@caprusit.com>
2013-04-08 14:47:12 +0530

    Hello World

---
+++ TestProject/src/com/ashok/sample/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ashok.sample;
+
+/**
+ * @author ashokram.adiraju
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
